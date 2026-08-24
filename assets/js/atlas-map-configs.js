35e8e1b331b304649f0ab591308f1ca998db7a81
yagenji <makoto.yagenji@gmail.com>
2026-08-24 21:12:21 +0900

    Add atlas map config registry

---
+++ assets/js/atlas-map-configs.js
@@ -0,0 +1,9 @@
+window.JOURNEY_ATLAS_MAP_CONFIGS = Object.freeze({
+  iceland: Object.freeze({
+    source: 'assets/images/iceland/map-atlas.svg',
+    bounds: Object.freeze({ west: -25.8, east: -12.2, south: 62.8, north: 67.1 }),
+    markerOffsets: Object.freeze({
+      hero: Object.freeze({ x: -0.8, y: 0 })
+    })
+  })
+});
